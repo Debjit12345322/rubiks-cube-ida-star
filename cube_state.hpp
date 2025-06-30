76af03b0691c354de9ccb6506f92e38f9ef1f2c4
Debjit12345322 <154426427+Debjit12345322@users.noreply.github.com>
2025-07-01 01:25:24 +0530

    Update cube_state.hpp

--- cube_state.hpp
+++ cube_state.hpp
@@ -2,15 +2,12 @@
 #include <array>
 #include "move.hpp"
 
-struct MoveEffect;
-
 struct CubeState {
     std::array<uint8_t, 8> corner_pos;
     std::array<uint8_t, 8> corner_orient;
     std::array<uint8_t, 12> edge_pos;
     std::array<uint8_t, 12> edge_orient;
 
-    void applyMove(Move move);
-    void applyMoveEffect(const MoveEffect& effect);
-    bool isSolved() const;
+    bool is_solved() const;
+    void apply_move(Move move);
 };
