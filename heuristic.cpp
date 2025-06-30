744910fb5d46f320e2ffed00e6382e52401e0f8e
Debjit12345322 <154426427+Debjit12345322@users.noreply.github.com>
2025-07-01 01:28:18 +0530

    Update heuristic.cpp

--- heuristic.cpp
+++ heuristic.cpp
@@ -1,21 +1,8 @@
 #include "heuristic.hpp"
 
 int heuristic(const CubeState& cube) {
-    int corner_twist = 0;
-    for (int c : cube.corner_orient) {
-        if (c != 0) {
-            corner_twist = 1;
-            break;
-        }
-    }
-
-    int edge_flip = 0;
-    for (int e : cube.edge_orient) {
-        if (e != 0) {
-            edge_flip = 1;
-            break;
-        }
-    }
-
-    return corner_twist + edge_flip;
+    int score = 0;
+    for (auto c : cube.corner_orient) if (c != 0) ++score;
+    for (auto e : cube.edge_orient) if (e != 0) ++score;
+    return score;
 }
